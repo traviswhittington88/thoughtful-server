a443409b102d223ba9b02b156d01bf5a52fd4123
traviswhittington88 <traviswhittington@ymail.com>
2020-01-20 22:32:29 -0600

    added GET /api/entry/entry_id endpoint

--- src/entries/entries-router.js
+++ src/entries/entries-router.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express')
 const EntriesService = require('../entries/entries-service')
 const { requireAuth } = require('../middleware/jwt-auth')
 
 const entriesRouter = express.Router()
+const jsonBodyParser = express.json()
 
 entriesRouter
   .route('/')
@@ -13,13 +15,69 @@ entriesRouter
       })
       .catch(next)
   })
+  .post(jsonBodyParser, (req, res, next) => {
+    const { title, content, journal_id } = req.body
+    const newEntry = { title, content, journal_id } 
+
+    for (const [key, value] of Object.entries(newEntry)) {
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+    }
+
+    EntriesService.insertEntry(
+      req.app.get('db'),
+      newEntry
+    )
+    .then(entry => {
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `/${entry.id}`))
+        .json(EntriesService.serializeEntry(entry))
+    })
+    .catch(next)
+  })
 
   entriesRouter
     .route('/:entry_id')
-    .all(requireAuth)
-    .get((req, res) => {
+    //.all(requireAuth)
+    .get((req, res, next) => {
+      EntriesService.getById(
+        req.app.get('db'),
+        req.params.entry_id
+      )
+      .then(entry => {
+        if (!entry) {
+          res.status(400).json({
+            error: {
+              message: `Entry does not exist`
+            }
+          })
+        }
+        res.entry = entry
+        next()
+      })
+      .catch(next)
+    })
+    .get((req, res, next) => {
       res.json(EntriesService.serializeEntry(res.entry))
     })
+    .delete((req, res, next) => {
+      EntriesService.deleteEntry(
+        req.app.get('db'),
+        req.params.entry_id
+      )
+      .then(numOfRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+    })
+    .patch(jsonBodyParser, (req, res, next) => {
+      
+    })
+
+  
   
 module.exports = entriesRouter
   

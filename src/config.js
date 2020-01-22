d7af080adfb8ef3860aa1be4b3b7ffcd210dd31b
traviswhittington88 <traviswhittington@ymail.com>
2020-01-22 14:59:53 -0600

    deployed to production via heroku app

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://thoughtful@localhost/thoughtful',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://thoughtful@localhost/thoughtful',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postresql://thoughtful@localhost/thoughtful-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
 }

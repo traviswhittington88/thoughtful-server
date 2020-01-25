8e046d0231d324ec4a99b1294dbcb1afd9fd11ce
traviswhittington88 <traviswhittington@ymail.com>
2020-01-24 20:02:30 -0600

    removed the 20s expiry on JWT and moved to 1hr

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://thoughtful@localhost/thoughtful',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postresql://thoughtful@localhost/thoughtful-test',
     JWT_SECRET: process.env.JWT_SECRET || 'thoughtful-client-auth-token',
-    JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '1hr',
 }
